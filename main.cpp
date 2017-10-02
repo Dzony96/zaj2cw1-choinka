e4b5d7716145296e89c581b68b17c6d2679895fa
Dzony96 <32269577+Dzony96@users.noreply.github.com>
2017-10-02 17:19:34 +0200

    choinka

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Podaj liczbe wierszy: "<<endl;
+    cin>>n;
+    for(int i=0; i<40; i++);
+    (
+     for(int j=0; j<40; j++);
+     (
+      if(i<=20-j)&&(j>=20-i)
+        cout<<"*";
+        else cout<<" ";
+      )
+     )
+
+
+    return 0;
+}
